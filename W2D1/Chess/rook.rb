ea333b2013cc26827b52040273b2bb0329d10815
Adrian Rivero <adrivero89@gmail.com>
2017-08-08 17:53:31 -0700

    almost done with chess

--- W2D1/Chess/rook.rb
+++ W2D1/Chess/rook.rb
@@ -1,8 +1,17 @@
 require_relative 'piece'
+require_relative 'module'
 
 class Rook < Piece
-  def initialize(color, pos)
+  include SlidingPiece
+
+  def initialize(color, pos, board)
     @sym = "R"
+    @move_dir = [
+    [0,1],
+    [1,0],
+    [0,-1],
+    [-1,0]
+    ]
     super
   end
 end
